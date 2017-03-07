8adb77451577b9a1eedf7190e985789e0cdb3dac
HoldenVh <hhiggins@stuy.edu>
2017-03-07 15:35:09 -0500

    new

---
+++ 05USACO/USACO.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class USACO{
+    public USACO(){}
+
+    public int bronze(String filename){
+	File text=new File(filename);
+	Scanner field = new Scanner(text);
+	String line=field.nextLine();
+	String[] parts = line.split(" ");
+	int[][] board=new int[Interger.parseInt(line)
+    }
+
+    public int silver(String filename){}
+}
+
+
+
