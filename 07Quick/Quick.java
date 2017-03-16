194e88013f20d61cacf4bc4a319c3834cb3de90d
Holden Higgins <hhiggins@stuy.edu>
2017-03-16 03:14:49 -0400

    init

---
+++ 07Quick/Quick.java
@@ -0,0 +1,11 @@
+public class Quick{
+
+    public static int[] partition(int[] data){
+    }
+
+    public static int quickselect(int []data, int k){
+	//return the value that is the kth smallest value of the array. 
+	//use your partition method to help you accomplish this.
+    }
+
+}
